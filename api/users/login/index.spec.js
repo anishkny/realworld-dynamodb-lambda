e6f45d3404d733968701bdda714be2834afa53d5
Arto Liukkonen <arto@51.fi>
2017-06-07 21:01:47 +0300

    Refactor initial handlers. Add Jest for tests. (#3)
    - Register stores user in DB
     - Login validates data and returns user object
     - Doesn't yet return auth token to frontend, so login doesn't actually work

---
+++ api/users/login/index.spec.js
@@ -0,0 +1,28 @@
+const login = require('./index');
+
+test('user login fails - empty credentials', done => {
+  let callback = (data) => {
+    expect(data.errors.User).toEqual(['is required.']);
+    done();
+  }
+
+  login.respond({}, callback);
+});
+
+test('user login fails - wrong credentials', done => {
+  let callback = (data, foo) => {
+    expect(data.errors.Error).toEqual(['validating user.']);
+    done();
+  }
+
+  login.respond({body: JSON.stringify({ user: { email: 'foo@bar.com', password: 'baz' }})}, callback);
+});
+
+test('user login successful', done => {
+  let callback = (error, data) => {
+    expect(data.user.email).toBe('john@jacob.com');
+    done();
+  }
+
+  login.respond({body: JSON.stringify({ user: { email: 'john@jacob.com', password: 'johnnyjacob' }})}, callback);
+});
---
+++ api/users/register/index.js
@@ -0,0 +1,45 @@
+const aws = require('aws-sdk');
+const dynamoDb = new aws.DynamoDB.DocumentClient();
+const { computeHash } = require('../../../lib/helpers');
+
+module.exports.respond = function (event, cb) {
+  var data = event.body ? JSON.parse(event.body) : event;
+
+  if (!data.user) {
+    cb({ statusCode: 422, errors: { User: ["is required."] } });
+    return;
+  }
+
+  // TODO: Validate email format
+  // TODO: Validate password requirements
+  var email = data.user.email;
+  var password = data.user.password;
+  if (!email || !password) {
+    cb({ statusCode: 422, errors: { "Email and password": ["are required."] } });
+    return;
+  }
+
+  const hashedPassword = computeHash(password);
+
+  var params = {
+    TableName: 'usersTable',
+    Item: {
+      email: email,
+      password: hashedPassword.hash,
+      salt: hashedPassword.salt
+    },
+    ConditionExpression: 'attribute_not_exists (email)'
+  };
+
+  dynamoDb.put(params, function (error, result) {
+    if (error) {
+      console.error(error);
+      cb({ statusCode: 422, errors: { Error: ["validating user."] } });
+      return;
+    }
+
+    cb(null, {
+      created: true
+    });
+  });
+}
