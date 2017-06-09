18847a87b3a08df9cc7370ecd298ceeb31c9a77d
Arto Liukkonen <arto@51.fi>
2017-06-09 13:33:42 +0300

    Return JWT token on login

--- api/users/login/index.js
+++ api/users/login/index.js
@@ -1,5 +1,10 @@
+const config = require('../../../config');
 const aws = require('aws-sdk');
+aws.config.update({
+  region: config.region || process.env.AWS_REGION || 'eu-west-1'
+});
 const dynamoDb = new aws.DynamoDB.DocumentClient();
+const jwt = require('jsonwebtoken');
 const { computeHash } = require('../../../lib/helpers');
 
 module.exports.respond = function (event, cb) {
@@ -32,13 +37,11 @@ module.exports.respond = function (event, cb) {
 
     let hash = computeHash(password, result.Item.salt);
 
-    // TODO: Calculate & return CognitoIdentity token
-    //       This requires setting up identity pool (via script or through AWS Console)
     if(hash === result.Item.password) {
       cb(null, {
         user: {
           id: 'todo',
-          token: 'todo',
+          token: jwt.sign({ email: result.Item.email }, config.JWT_SECRET),
           email: result.Item.email,
           username: result.Item.username,
           image: result.Item.image,
