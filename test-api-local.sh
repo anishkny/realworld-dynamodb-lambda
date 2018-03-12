7da8ea2836ccf4f9598ad33f2da40285a3a47f1c
Anish Karandikar <anishkny@gmail.com>
2018-03-12 19:08:56 -0400

    Add test-api-local.sh script

---
+++ test-api-local.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -x
+export PORT=3000
+lsof -ti:$PORT | xargs kill
+
+set -e
+nyc serverless offline &
+export SLS_PID=$!
+sleep 5
+API_URL=http://localhost:$PORT mocha
+kill $SLS_PID
