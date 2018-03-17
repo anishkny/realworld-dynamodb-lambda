4a3540b6fd7d93547b893d0022ebcd8ba7408fc4
Anish Karandikar <anishkny@gmail.com>
2018-03-16 23:34:21 -0400

    Add DynamoDB local

--- test-api-local.sh
+++ test-api-local.sh
@@ -5,10 +5,12 @@ lsof -ti:$PORT | xargs kill
 rm -rf .nyc_output/ coverage/
 
 set -e
+sls dynamodb start --migrate &
+export DDB_PID=$!
 nyc serverless offline &
 export SLS_PID=$!
 sleep 5
 API_URL=http://localhost:$PORT mocha
 sleep 5
-kill $SLS_PID
+kill $SLS_PID $DDB_PID
 sleep 5
