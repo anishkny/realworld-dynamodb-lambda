0dc4a9d1ddc084b26b56c6302ae74cfcebda1277
Anish Karandikar <anishkny@gmail.com>
2018-05-07 16:05:25 -0400

    chore(test): Use github-markdown-css for network dump

--- test-api-local.sh
+++ test-api-local.sh
@@ -12,16 +12,26 @@ API_URL=http://localhost:3000 mocha 2>&1 | tee .test_output/test.log
 set +e
 sleep 5
 
-showdown makehtml -i $NETWORK_DUMP_FILE -o .test_output/network.html
-cat >> .test_output/network.html << EOF
+cat > .test_output/network.html << EOF
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="//rawgit.com/sindresorhus/github-markdown-css/gh-pages/github-markdown.css">
 <style>
-  body { box-sizing: border-box; min-width: 200px; max-width: 980px; margin: 0 auto; padding: 45px; }
-  @media (max-width: 767px) { body { padding: 15px; } }
-  * { color: #24292e; }
-  h2 { font-family: Arial; }
-  pre { background-color: #f6f8fa; padding: 1em; }
+	.markdown-body {
+		box-sizing: border-box;
+		min-width: 200px;
+		max-width: 980px;
+		margin: 0 auto;
+		padding: 45px;
+	}
+
+	@media (max-width: 767px) {
+		.markdown-body {
+			padding: 15px;
+		}
+	}
 </style>
+<article class="markdown-body">
 EOF
+showdown makehtml --input $NETWORK_DUMP_FILE >> .test_output/network.html
 
 ./stop-server.sh
