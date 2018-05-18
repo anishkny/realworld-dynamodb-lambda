fe268140ff45c69ec7edeb250a2441ae360ffe3b
Anish Karandikar <anishkny@gmail.com>
2018-05-18 18:58:55 -0400

    test(reporter): Move axios interceptors out of TestHooks

--- test/TestHooks.js
+++ test/TestHooks.js
@@ -1,8 +1,6 @@
 const axios = require('axios');
-const fs = require('fs');
 const TestUtil = require('./TestUtil');
 const API_URL = process.env.API_URL;
-const NETWORK_DUMP_FILE = process.env.NETWORK_DUMP_FILE;
 
 // Root level 'before' function called at beginning of suite
 before(async () => {
@@ -23,38 +21,6 @@ before(async () => {
 
       return config;
     });
-  }
-
-  if (NETWORK_DUMP_FILE) {
-
-    axios.interceptors.request.use(async (config) => {
-      if (config.data) {
-        const reqDump = '```\n' +
-          `${config.method.toUpperCase()} ${config.url}\n\n` +
-          JSON.stringify(config.data, null, 2) + '\n' +
-          '```\n';
-        fs.appendFileSync(NETWORK_DUMP_FILE, reqDump);
-      }
-
-      return config;
-    });
-
-    axios.interceptors.response.use(async (response) => {
-      const resDump = '```\n' +
-        `${response.status} ${response.statusText}\n\n` +
-        JSON.stringify(response.data, null, 2) + '\n' +
-        '```\n';
-      fs.appendFileSync(NETWORK_DUMP_FILE, resDump);
-      return response;
-    }, async (error) => {
-      const resDump = '```\n' +
-        `${error.response.status} ${error.response.statusText}\n\n` +
-        JSON.stringify(error.response.data, null, 2) + '\n' +
-        '```\n';
-      fs.appendFileSync(NETWORK_DUMP_FILE, resDump);
-      return Promise.reject(error);
-    });
-
   }
 
 });
--- test/mocha-axios-reporter.js
+++ test/mocha-axios-reporter.js
@@ -1,14 +1,43 @@
 const mocha = require('mocha');
+const axios = require('axios');
 const fs = require('fs');
 const NETWORK_DUMP_FILE = process.env.NETWORK_DUMP_FILE;
 
 module.exports = MochaAxiosReporter;
-let indent = 1; // eslint-disable-line no-unused-vars
+let indent = 0; // eslint-disable-line no-unused-vars
 
 function MochaAxiosReporter(runner) {
   mocha.reporters.Spec.call(this, runner);
 
   if (NETWORK_DUMP_FILE) {
+
+    axios.interceptors.request.use(async (config) => {
+      let reqDump = '```\n' + `${config.method.toUpperCase()} ${config.url}\n`;
+      if (config.data) {
+        reqDump += '\n' + JSON.stringify(config.data, null, 2) + '\n';
+      }
+      reqDump += '```\n';
+      fs.appendFileSync(NETWORK_DUMP_FILE, reqDump);
+
+      return config;
+    });
+
+    axios.interceptors.response.use(async (response) => {
+      const resDump = '```\n' +
+        `${response.status} ${response.statusText}\n\n` +
+        JSON.stringify(response.data, null, 2) + '\n' +
+        '```\n';
+      fs.appendFileSync(NETWORK_DUMP_FILE, resDump);
+      return response;
+    }, async (error) => {
+      const resDump = '```\n' +
+        `${error.response.status} ${error.response.statusText}\n\n` +
+        JSON.stringify(error.response.data, null, 2) + '\n' +
+        '```\n';
+      fs.appendFileSync(NETWORK_DUMP_FILE, resDump);
+      return Promise.reject(error);
+    });
+
     runner.on('suite', suite => {
       if (!suite || !suite.title) {
         return;
